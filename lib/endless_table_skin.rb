98d84820012769559938c3f93ef652c6c20a04f9
Kadvin <me@kadvin.com>
2010-12-21 11:31:57 +0800

    Refer to the action defined in actions instead of activerecord i18n scope

--- lib/endless_table_skin.rb
+++ lib/endless_table_skin.rb
@@ -4,21 +4,21 @@ module EndlessTableSkin
   mattr_reader :default_actions, :html_options
   @@default_actions = ActiveSupport::OrderedHash.new #[{:new => {}}, :edit, :delete, :refresh]
   @@html_options = { :check_interval=>1, :scroll_threshold=>50, :error_threshold=>3, :outer_height=>200 }
-  @@default_actions[:new]     = {:label => I18n.t("activerecord.actions.new"),
+  @@default_actions[:new]     = {:label => I18n.t("actions.new"),
     :url=>"%s/new", :image=>"/images/endless/new.png",
     :onclick => "Endless.Button.activateNew(this)"
   }
-  @@default_actions[:edit]    = {:label => I18n.t("activerecord.actions.edit"),
+  @@default_actions[:edit]    = {:label => I18n.t("actions.edit"),
     :url=>"%s/:id/edit", :image=>"/images/endless/edit.png",
     :onSelectionChanged =>"Endless.Button.adjustForOne(source,event)",
     :onclick => "Endless.Button.activateEdit(this)"
   }
-  @@default_actions[:delete]  = {:label => I18n.t("activerecord.actions.delete"),
+  @@default_actions[:delete]  = {:label => I18n.t("actions.delete"),
     :url=>"%s/:id", :image=>"/images/endless/delete.png",
     :onSelectionChanged =>"Endless.Button.adjustForMore(source,event)",
     :onclick => "Endless.Button.activateDelete(this)"
   }
-  @@default_actions[:refresh] = {:label => I18n.t("activerecord.actions.refresh"),
+  @@default_actions[:refresh] = {:label => I18n.t("actions.refresh"),
     :url=>"%s", :image=>"/images/endless/refresh.png",
     :onclick => "Endless.Button.activateRefresh(this)"
   }
